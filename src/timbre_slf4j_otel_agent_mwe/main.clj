921ec62ff09160d5a0c233e7ad17ddfd5e93839d
Dennis Schridde <dennis@metabase.com>
2025-01-10 19:28:40 +0100

    Show workaround for interaction Java agent <-> SLF4J <-> Timbre

--- src/timbre_slf4j_otel_agent_mwe/main.clj
+++ src/timbre_slf4j_otel_agent_mwe/main.clj
@@ -9,6 +9,11 @@
   []
   ;; Without the following line, logs from the OpenTelemetry Java agent will not be passed through Timbre:
   (LoggerFactory/getLogger (str *ns*))
+  ;; Move the call to `org.slf4j.LoggerFactory#getLogger(java.lang.String)` after `taoensso.timbre/merge-config!`
+  ;; to show that Timbre would process the agent's log lines if it was configured early enough.
+  ;; Sadly, this does not work for real applications,
+  ;; where we are not in control over when the first call to SLF4J happens,
+  ;; since libraries we use might call it before us.
   (println ">>> STARTUP LOG CONFIG" log/*config*)
   (log/info "STARTING")
   (log/merge-config! {:min-level [[#{"timbre-slf4j-otel-agent-mwe.*"} :debug]
