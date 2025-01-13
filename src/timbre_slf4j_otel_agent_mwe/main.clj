e1d1505b7e5d6ea16d121166038134f4f28d6500
Dennis Schridde <dennis@metabase.com>
2025-01-13 14:16:03 +0100

    Print Timbre config earlier
    References: https://clojurians.slack.com/archives/C1J6MQC2H/p1736547095711989?thread_ts=1736514948.165309&cid=C1J6MQC2H

--- src/timbre_slf4j_otel_agent_mwe/main.clj
+++ src/timbre_slf4j_otel_agent_mwe/main.clj
@@ -13,21 +13,22 @@
     (when (and (sequential? compile-time-min-level)
                (= :timbre/invalid-min-level (first compile-time-min-level)))
       (throw (ex-info "invalid Timbre compile-time min-level" (:_init-config log/*config*)))))
-  ;; Without the following line, logs from the OpenTelemetry Java agent will not be passed through Timbre:
+  (println ">>> COMPILE-TIME MIN-LEVEL" (:min-level log/*config*))
+  (println ">>> COMPILE-TIME CONFIG" log/*config*)
+  (log/info "STARTING")
+  ;; Without the following line, logs from the OpenTelemetry Java agent will not be passed through Timbre at all:
   (LoggerFactory/getLogger (str *ns*))
   ;; Move the call to `org.slf4j.LoggerFactory#getLogger(java.lang.String)` after `taoensso.timbre/merge-config!`
   ;; to show that Timbre would process the agent's log lines if it was configured early enough.
   ;; Sadly, this does not work for real applications,
   ;; where we are not in control over when the first call to SLF4J happens,
   ;; since libraries we use might call it before us.
-  (println ">>> STARTUP LOG CONFIG" log/*config*)
-  (log/info "STARTING")
   (log/merge-config! {:min-level [[#{"timbre-slf4j-otel-agent-mwe.*"} :debug]
                                   [#{"*"} :info]]
                       :appenders {:println {:min-level [[#{"timbre-slf4j-otel-agent-mwe.*"} :debug]
                                                         [#{"*"} :info]]}}
                       :middleware [(fn [data] (println ">>> TIMBRE DEBUG" data) data)]})
-  (println ">>> INITIALIZED LOG CONFIG" log/*config*)
+  (println ">>> RUNTIME CONFIG" log/*config*)
   (log/info "LOGGER INITIALIZED")
   (Thread/sleep 10000)
   (log/info "EXITING"))
