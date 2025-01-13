6d2b3692e1551d80128d68a559af8fa07c98e20e
Dennis Schridde <dennis@metabase.com>
2025-01-13 17:37:29 +0100

    Fix filtering OpenTelemetry Java agent logs by Timbre min-level

--- build.sh
+++ build.sh
@@ -3,8 +3,4 @@
 set -eu
 set -o pipefail
 
-# This is not persisted into the JAR,
-# i.e. if you don't also set this at runtime, Timbre will see `:compile-time-config {:min-level nil}` at runtime:
-export TAOENSSO_TIMBRE_MIN_LEVEL_EDN='[[#{"timbre-slf4j-otel-agent-mwe.*"} :debug][#{"*"} :info]]'
-
 exec clj -T:build uber
--- run.sh
+++ run.sh
@@ -5,10 +5,8 @@ set -o pipefail
 
 # https://opentelemetry.io/docs/zero-code/java/agent/configuration/#java-agent-logging-output
 export OTEL_JAVAAGENT_LOGGING=application
-# https://github.com/taoensso/timbre/wiki/1-Getting-started#compile-time-elision
-# The environment variable does not use the syntax described in the docstring of `taoensso.timbre/*config*`:
-#export TAOENSSO_TIMBRE_MIN_LEVEL_EDN='[[#{"timbre-slf4j-otel-agent-mwe.*"} :debug][#{"*"} :info]]'
-export TAOENSSO_TIMBRE_MIN_LEVEL_EDN=':info'
+# https://github.com/taoensso/timbre/wiki/1-Getting-started#configuration
+export TAOENSSO_TIMBRE_CONFIG_EDN='{:min-level [[#{"timbre-slf4j-otel-agent-mwe.*"} :debug][#{"*"} :info]]}'
 
 jar="$(find target -name '*-standalone.jar' | head -n1)"
 
