d566b721a416015182e8f26dbcc545adb24722f8
Dennis Schridde <dennis@metabase.com>
2025-01-13 14:30:38 +0100

    Configure middleware earlier

--- src/timbre_slf4j_otel_agent_mwe/main.clj
+++ src/timbre_slf4j_otel_agent_mwe/main.clj
@@ -15,20 +15,22 @@
       (throw (ex-info "invalid Timbre compile-time min-level" (:_init-config log/*config*)))))
   (println ">>> COMPILE-TIME MIN-LEVEL" (:min-level log/*config*))
   (println ">>> COMPILE-TIME CONFIG" log/*config*)
-  (log/info "STARTING")
+  ;; With the following line before the call of `org.slf4j.LoggerFactory#getLogger(java.lang.String)`,
+  ;; we see that Timbre processes the log messages from the OpenTelemetry Java agent:
+  (log/merge-config! {:middleware [(fn [data] (println ">>> HELLO FROM TIMBRE MIDDLEWARE" data) data)]})
+  (println ">>> RUNTIME CONFIG WITH MIDDLEWARE" log/*config*)
   ;; Without the following line, logs from the OpenTelemetry Java agent will not be passed through Timbre at all:
   (LoggerFactory/getLogger (str *ns*))
-  ;; Move the call to `org.slf4j.LoggerFactory#getLogger(java.lang.String)` after `taoensso.timbre/merge-config!`
-  ;; to show that Timbre would process the agent's log lines if it was configured early enough.
+  ;; Move the following line call to before `org.slf4j.LoggerFactory#getLogger(java.lang.String)`
+  ;; to show that Timbre would filter the agent's log lines by level if it was configured early enough.
   ;; Sadly, this does not work for real applications,
   ;; where we are not in control over when the first call to SLF4J happens,
   ;; since libraries we use might call it before us.
   (log/merge-config! {:min-level [[#{"timbre-slf4j-otel-agent-mwe.*"} :debug]
                                   [#{"*"} :info]]
                       :appenders {:println {:min-level [[#{"timbre-slf4j-otel-agent-mwe.*"} :debug]
-                                                        [#{"*"} :info]]}}
-                      :middleware [(fn [data] (println ">>> TIMBRE DEBUG" data) data)]})
-  (println ">>> RUNTIME CONFIG" log/*config*)
-  (log/info "LOGGER INITIALIZED")
+                                                        [#{"*"} :info]]}}})
+  (println ">>> RUNTIME CONFIG WITH MIN-LEVEL" log/*config*)
+  (log/info "STARTED")
   (Thread/sleep 10000)
   (log/info "EXITING"))
