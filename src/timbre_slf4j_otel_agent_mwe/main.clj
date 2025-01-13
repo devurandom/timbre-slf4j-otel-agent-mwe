2cc31776a9608512e106da56e10324cc8230fa1e
Dennis Schridde <dennis@metabase.com>
2025-01-13 14:08:30 +0100

    Throw on invalid Timbre compile-time-config

--- src/timbre_slf4j_otel_agent_mwe/main.clj
+++ src/timbre_slf4j_otel_agent_mwe/main.clj
@@ -7,6 +7,12 @@
 
 (defn -main
   []
+  ;; Throw like recent versions of Timbre would,
+  ;; cf. https://github.com/taoensso/timbre/commit/a393582b5d90ef019f9e47e1f3fcb16c2c23ff3a:
+  (when-let [compile-time-min-level (get-in log/*config* [:_init-config :compile-time-config :min-level])]
+    (when (and (sequential? compile-time-min-level)
+               (= :timbre/invalid-min-level (first compile-time-min-level)))
+      (throw (ex-info "invalid Timbre compile-time min-level" (:_init-config log/*config*)))))
   ;; Without the following line, logs from the OpenTelemetry Java agent will not be passed through Timbre:
   (LoggerFactory/getLogger (str *ns*))
   ;; Move the call to `org.slf4j.LoggerFactory#getLogger(java.lang.String)` after `taoensso.timbre/merge-config!`
